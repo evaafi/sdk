d2eb38b93a9405d633df8cfd9e890c829d1e69ec
Arsen Andrian <imcrazy.sixtouch@yandex.ru>
2024-09-05 03:03:19 +0300

    fix: predict health factor

--- src/api/math.ts
+++ src/api/math.ts
@@ -269,8 +269,7 @@ export function predictHealthFactor(args: PredictHealthFactorArgs): number {
     const currentBalance = assetPrice * Number(currentAmount) / Math.pow(10, decimals);
     const changeType = args.balanceChangeType;
 
-    if (currentAmount != null && !Number.isNaN(currentAmount) &&
-             Number.isFinite(currentAmount) && currentAmount != 0n) { 
+    if (currentAmount != null && currentAmount != 0n) { 
         if (changeType == BalanceChangeType.Borrow) {
             totalBorrow += currentBalance * (1 + Number(assetConfig.originationFee) / Number(args.masterConstants.ASSET_ORIGINATION_FEE_SCALE));
         } else if (changeType == BalanceChangeType.Repay) {
