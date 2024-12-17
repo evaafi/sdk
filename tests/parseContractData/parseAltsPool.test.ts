8c64604f23a776727b90ce80748d70671d57aaf4
Arsen Andrian <imcrazy.sixtouch@yandex.ru>
2024-12-17 18:08:02 +0300

    additional tests, fix bug in the feature ton version,

--- tests/parseContractData/parseAltsPool.test.ts
+++ tests/parseContractData/parseAltsPool.test.ts
@@ -1,4 +1,4 @@
-import { calculatePresentValue, DOGS_MAINNET, Evaa, EvaaUser, MAINNET_ALTS_POOL_CONFIG, MAINNET_POOL_CONFIG, presentValue, Prices, PricesCollector, RawPriceData, UserDataActive, verifyPricesSign, verifyRawPriceDataSign } from "../../src";
+import { calculatePresentValue, DOGS_MAINNET, Evaa, EvaaUser, MAINNET_ALTS_POOL_CONFIG, MAINNET_POOL_CONFIG, NOT_MAINNET, presentValue, Prices, PricesCollector, RawPriceData, UserDataActive, verifyPricesSign, verifyRawPriceDataSign } from "../../src";
 import { Address, beginCell, Builder, Dictionary, OpenedContract, TonClient } from "@ton/ton";
 
 import dotenv from 'dotenv';
@@ -25,6 +25,22 @@ describe('parseAltsData test', () => {
         
         const result = await clientMainNet.runMethod(MAINNET_ALTS_POOL_CONFIG.masterAddress, 'getAssetTotals', [{ type: "int", value: DOGS_MAINNET.assetId }])
         const assetData = evaaMainNet.data?.assetsData.get(DOGS_MAINNET.assetId)!;
+        const totalSupplyPrincipal = assetData.totalSupply;
+
+        const totalAmount = calculatePresentValue(assetData.sRate, totalSupplyPrincipal, MAINNET_ALTS_POOL_CONFIG.masterConstants);
+        let diff = totalAmount - result.stack.readBigNumber();  // first is total suuply second is total borrow
+        if (diff < 0) {
+            diff = -diff;
+        }
+        expect(diff).toBeLessThan(2_000_000_000n);
+    });
+
+
+    test('test NOT', async () => {
+        expect.assertions(1);
+        
+        const result = await clientMainNet.runMethod(MAINNET_ALTS_POOL_CONFIG.masterAddress, 'getAssetTotals', [{ type: "int", value: NOT_MAINNET.assetId }])
+        const assetData = evaaMainNet.data?.assetsData.get(NOT_MAINNET.assetId)!;
         const totalSupplyPrincipal = assetData.totalSupply;
 
         const totalAmount = calculatePresentValue(assetData.sRate, totalSupplyPrincipal, MAINNET_ALTS_POOL_CONFIG.masterConstants);
