1aeae3819a64a34127c43ccfeddaf865accad82b
Danil Shaymurzin <im@murka.me>
2025-03-28 16:03:57 +0500

    refactor(EvaaUserRewards): simplify class and openUserContract method

--- src/rewards/EvaaUserRewards.ts
+++ src/rewards/EvaaUserRewards.ts
@@ -1,29 +1,25 @@
-import { Address, beginCell, Cell, Contract, ContractProvider, Sender } from '@ton/ton';
+import { Address, Contract, ContractProvider } from '@ton/ton';
 import { EvaaUserRewardsConfig } from '../types/UserRewards';
 import { RewardUser } from './RewardUser';
 
 export class EvaaUserRewards implements Contract {
     readonly address: Address;
-    constructor(readonly config: EvaaUserRewardsConfig) {
-        this.address = config.userAddress;
+    constructor(
+        readonly userAddress: Address,
+        readonly config: EvaaUserRewardsConfig,
+    ) {
+        this.address = this.userAddress;
     }
 
-    private openUserContract(provider: ContractProvider) {
-        return provider.open(RewardUser.createFromConfig(this.config));
-    }
-
-    async sendDeployAndClaim(provider: ContractProvider, via: Sender, signedClaim: string, claimBody: string) {
-        const claimCell = beginCell()
-            .storeUint(2, 32)
-            .storeUint(0, 64)
-            .storeBuffer(Buffer.from(signedClaim, 'base64'))
-            .storeRef(Cell.fromBase64(claimBody))
-            .endCell();
-
-        const dataCell = RewardUser.rewardUserConfigToCell(this.config);
-        const stateInit = beginCell().storeRef(this.config.rewardUserCode).storeRef(dataCell).endCell();
-
-        const combinedCell = beginCell().storeRef(stateInit).storeRef(claimCell).endCell();
-        await this.openUserContract(provider).sendClaim(via, combinedCell);
+    openUserContract(provider: ContractProvider) {
+        return provider.open(
+            RewardUser.createFromConfig({
+                asset: this.config.asset,
+                rewardMasterAddress: this.config.rewardMasterAddress,
+                rewardUserCode: this.config.rewardUserCode,
+                userAddress: this.userAddress,
+                publicKey: this.config.publicKey,
+            }),
+        );
     }
 }
