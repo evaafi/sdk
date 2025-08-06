5206e2416ffd73d4458b3d25774d0b7018390464
sepezho <47150504+sepezho@users.noreply.github.com>
2025-08-06 13:53:16 +0200

    tgbtc new version (shard optimized jettons)

--- src/utils/userJettonWallet.ts
+++ src/utils/userJettonWallet.ts
@@ -42,7 +42,6 @@ function getUserJettonData(ownerAddress: Address, assetName: string, jettonWalle
               .endCell();
       case 'tgBTC':
           return beginCell()
-              .storeUint(0, 4)
               .storeCoins(0)
               .storeAddress(ownerAddress)
               .storeAddress(jettonMasterAddress)
@@ -55,11 +54,13 @@ function getUserJettonData(ownerAddress: Address, assetName: string, jettonWalle
               .endCell();
   }
 }
+
 export function getUserJettonWallet(ownerAddress: Address, poolAssetConfig: PoolAssetConfig) {
   const assetName = poolAssetConfig.name;
   if (assetName == 'TON' || poolAssetConfig.assetId === UNDEFINED_ASSET.assetId) {
     throw new Error(`Cant getUserJettonWallet for ${poolAssetConfig.name} asset`)
   }
+  
   let jettonWalletCode = poolAssetConfig.jettonWalletCode;
 
   if (assetName === 'USDT' || assetName === 'tsTON') {
@@ -69,14 +70,44 @@ export function getUserJettonWallet(ownerAddress: Address, poolAssetConfig: Pool
 
   const jettonData = getUserJettonData(ownerAddress, assetName, jettonWalletCode, poolAssetConfig.jettonMasterAddress);
 
-  const stateInit = beginCell()
-    .store(
-      storeStateInit({
-        code: jettonWalletCode,
-        data: jettonData
-      })
-    )
-    .endCell();
+  if (assetName === 'tgBTC') {
+    const SHARD_DEPTH = 8;
+    const MY_WORKCHAIN = 0;
+    const shardPrefix = beginCell().storeAddress(ownerAddress).endCell().beginParse().skip(3 + 8).preloadUint(SHARD_DEPTH);
+    
+    const stateInit = beginCell()
+       .storeUint(1, 1)
+       .storeUint(SHARD_DEPTH, 5) // No split_depth; No special
+       .storeUint(0, 1)
+       .storeMaybeRef(jettonWalletCode)
+       .storeMaybeRef(jettonData)
+       .storeUint(0, 1) // Empty libraries
+       .endCell();
+    
+    const cellHash = BigInt("0x" + stateInit.hash().toString('hex'));
 
-  return new Address(0, stateInit.hash());
+    const mask = (1n << BigInt(256 - SHARD_DEPTH)) - 1n;
+    const prefixLess = cellHash & mask;
+
+    const resultAddr = beginCell()
+      .storeUint(4, 3)                      // addr_std$10, 3 бита
+      .storeInt(MY_WORKCHAIN, 8)            // workchain_id, 8 бит
+      .storeUint(shardPrefix, SHARD_DEPTH)  // shard_prefix
+      .storeUint(prefixLess, 256 - SHARD_DEPTH) // остаток от init хэша
+      .endCell().beginParse().loadAddress();
+    
+    return resultAddr;
+
+  } else {
+    const stateInit = beginCell()
+      .store(
+        storeStateInit({
+          code: jettonWalletCode,
+          data: jettonData
+        })
+      )
+      .endCell();
+
+    return new Address(0, stateInit.hash());
+  }
 }
