5973b5a976f6f79827a3fa14eeccad12f5b1ba6c
Danil Shaymurzin <im@murka.me>
2025-08-19 18:09:28 +0500

    refactor(constants): simplify assets and pools configs with backward compatibilities

---
+++ src/constants/assets/assetId.ts
@@ -0,0 +1,28 @@
+import { sha256Hash } from '../../utils/sha256BigInt';
+
+export const ASSET_ID = {
+    TON: sha256Hash('TON'),
+    USDT: sha256Hash('USDT'),
+    jUSDT: sha256Hash('jUSDT'),
+    jUSDC: sha256Hash('jUSDC'),
+    stTON: sha256Hash('stTON'),
+    tsTON: sha256Hash('tsTON'),
+    tgBTC: sha256Hash('tgBTC'),
+    USDe: sha256Hash('USDe'),
+    tsUSDe: sha256Hash('tsUSDe'),
+    PT_tsUSDe_01Sep2025: sha256Hash('PT_tsUSDe_01Sep2025'),
+
+    // LP
+    TONUSDT_DEDUST: sha256Hash('TONUSDT_DEDUST'),
+    TONUSDT_STONFI: sha256Hash('TONUSDT_STONFI'),
+    TON_STORM: sha256Hash('TON_STORM'),
+    USDT_STORM: sha256Hash('USDT_STORM'),
+
+    // ALTS
+    NOT: sha256Hash('NOT'),
+    DOGS: sha256Hash('DOGS'),
+    CATI: sha256Hash('CATI'),
+
+    // Testnet assets
+    EUSDT: sha256Hash('EUSDT'),
+};
---
+++ src/constants/pools/mainnet.ts
@@ -0,0 +1,143 @@
+import { Address } from '@ton/core';
+import { PricesCollector } from '../../prices';
+import { PoolConfig } from '../../types/Master';
+import { EvaaRewardsConfig } from '../../types/MasterRewards';
+import {
+    CATI_MAINNET,
+    DOGS_MAINNET,
+    JUSDC_MAINNET,
+    JUSDT_MAINNET,
+    NOT_MAINNET,
+    PT_tsUSDe_01Sep2025_MAINNET,
+    STTON_MAINNET,
+    TON_MAINNET,
+    TON_STORM_MAINNET,
+    TONUSDT_DEDUST_MAINNET,
+    TSTON_MAINNET,
+    TSUSDE_MAINNET,
+    USDE_MAINNET,
+    USDT_MAINNET,
+    USDT_STORM_MAINNET,
+} from '../assets';
+import {
+    EVAA_ALTS_MAINNET,
+    EVAA_ALTS_MAINNET_VERSION,
+    EVAA_LP_MAINNET,
+    EVAA_LP_MAINNET_VERSION,
+    EVAA_MASTER_MAINNET,
+    EVAA_REWARDS_MASTER_CODE_MAINNET,
+    EVAA_REWARDS_USER_CODE_MAINNET,
+    EVAA_STABLE_MAINNET,
+    EVAA_TON_REWARDS_MASTER_MAINNET,
+    EVAA_USDT_REWARDS_MASTER_MAINNET,
+    LENDING_CODE,
+    MAINNET_VERSION,
+    MASTER_CONSTANTS,
+    ORACLES_ALTS,
+    ORACLES_LP,
+    ORACLES_MAINNET,
+    STABLE_VERSION,
+} from '../general';
+
+// Pool assets configs
+export const MAINNET_POOL_ASSETS_CONFIG = [
+    TON_MAINNET,
+    JUSDT_MAINNET,
+    JUSDC_MAINNET,
+    STTON_MAINNET,
+    TSTON_MAINNET,
+    USDT_MAINNET,
+    USDE_MAINNET,
+    TSUSDE_MAINNET,
+];
+
+const MAINNET_STABLE_POOL_ASSETS_CONFIG = [USDT_MAINNET, USDE_MAINNET, TSUSDE_MAINNET, PT_tsUSDe_01Sep2025_MAINNET];
+
+const MAINNET_LP_POOL_ASSETS_CONFIG = [
+    TON_MAINNET,
+    USDT_MAINNET,
+    TONUSDT_DEDUST_MAINNET,
+    TON_STORM_MAINNET,
+    USDT_STORM_MAINNET,
+];
+
+const MAINNET_ALTS_POOL_ASSETS_CONFIG = [TON_MAINNET, USDT_MAINNET, CATI_MAINNET, NOT_MAINNET, DOGS_MAINNET];
+
+export const MAINNET_POOL_CONFIG: PoolConfig = {
+    masterAddress: EVAA_MASTER_MAINNET,
+    masterVersion: MAINNET_VERSION,
+    masterConstants: MASTER_CONSTANTS,
+    oracles: new PricesCollector({
+        poolAssetsConfig: MAINNET_POOL_ASSETS_CONFIG,
+        minimalOracles: 3,
+        evaaOracles: ORACLES_MAINNET,
+    }),
+    poolAssetsConfig: MAINNET_POOL_ASSETS_CONFIG,
+    lendingCode: LENDING_CODE,
+};
+
+export const MAINNET_STABLE_POOL_CONFIG: PoolConfig = {
+    masterAddress: EVAA_STABLE_MAINNET,
+    masterVersion: STABLE_VERSION,
+    masterConstants: MASTER_CONSTANTS,
+    oracles: new PricesCollector({
+        poolAssetsConfig: MAINNET_STABLE_POOL_ASSETS_CONFIG,
+        minimalOracles: 3,
+        evaaOracles: ORACLES_MAINNET,
+    }),
+    poolAssetsConfig: MAINNET_STABLE_POOL_ASSETS_CONFIG,
+    lendingCode: LENDING_CODE,
+};
+
+export const MAINNET_LP_POOL_CONFIG: PoolConfig = {
+    masterAddress: EVAA_LP_MAINNET,
+    masterVersion: EVAA_LP_MAINNET_VERSION,
+    masterConstants: MASTER_CONSTANTS,
+    oracles: new PricesCollector({
+        poolAssetsConfig: MAINNET_LP_POOL_ASSETS_CONFIG,
+        minimalOracles: 3,
+        evaaOracles: ORACLES_LP,
+    }),
+    poolAssetsConfig: MAINNET_LP_POOL_ASSETS_CONFIG,
+    lendingCode: LENDING_CODE,
+};
+
+export const MAINNET_ALTS_POOL_CONFIG: PoolConfig = {
+    masterAddress: EVAA_ALTS_MAINNET,
+    masterVersion: EVAA_ALTS_MAINNET_VERSION,
+    masterConstants: MASTER_CONSTANTS,
+    oracles: new PricesCollector({
+        poolAssetsConfig: MAINNET_ALTS_POOL_ASSETS_CONFIG,
+        minimalOracles: 3,
+        evaaOracles: ORACLES_ALTS,
+    }),
+    poolAssetsConfig: MAINNET_ALTS_POOL_ASSETS_CONFIG,
+    lendingCode: LENDING_CODE,
+};
+
+export const MAINNET_MASTER_TON_REWARD_CONFIG: EvaaRewardsConfig = {
+    adminAddress: EVAA_TON_REWARDS_MASTER_MAINNET,
+    evaaMasterAddress: new Address(0, Buffer.alloc(32, 0)),
+    rewardMasterCode: EVAA_REWARDS_MASTER_CODE_MAINNET,
+    rewardUserCode: EVAA_REWARDS_USER_CODE_MAINNET,
+    asset: TON_MAINNET,
+    availableReward: 0,
+    publicKey: Buffer.from('bfe9a30221db4dff4c17e55d985e0b13a8f597bbb28002f311dc4429dad1ca95', 'hex'), // adminAddress publicKey
+};
+
+export const MAINNET_MASTER_USDT_REWARD_CONFIG: EvaaRewardsConfig = {
+    adminAddress: EVAA_USDT_REWARDS_MASTER_MAINNET,
+    evaaMasterAddress: new Address(0, Buffer.alloc(32, 0)),
+    rewardMasterCode: EVAA_REWARDS_MASTER_CODE_MAINNET,
+    rewardUserCode: EVAA_REWARDS_USER_CODE_MAINNET,
+    asset: USDT_MAINNET,
+    availableReward: 0,
+    publicKey: Buffer.from('9813725d6cead1c9bbc0e24b21d4fc62a7fa8ac4bb01b6758df30169a71dba67', 'hex'), // adminAddress publicKey
+};
+
+export const ALL_MAINNET_POOLS: PoolConfig[] = [
+    MAINNET_POOL_CONFIG,
+    MAINNET_LP_POOL_CONFIG,
+    MAINNET_ALTS_POOL_CONFIG,
+    MAINNET_STABLE_POOL_CONFIG,
+];
