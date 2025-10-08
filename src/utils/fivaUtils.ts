dde0d77a29cc7f02827b55f838836fd47e636984
Arsen Andrian <imcrazy.sixtouch@yandex.ru>
2025-10-08 17:47:04 +0800

    rm ununsed function

--- src/utils/fivaUtils.ts
+++ src/utils/fivaUtils.ts
@@ -21,9 +21,3 @@ function bigIntToBuffer(value: bigint, bits: number): Buffer {
   const hex = value.toString(16).padStart(bits / 4, '0');
   return Buffer.from(hex, 'hex');
 }
-
-export function getAddressShard(address: Address, isHex: boolean = true): string {
-  const curHashpart = BigInt('0x' + address.hash.toString('hex'));
-  const first8Bits = (curHashpart >> BigInt(256 - 8)) & BigInt(0xff);
-  return isHex ? first8Bits.toString(16).padStart(2, '0') : first8Bits.toString(2).padStart(8, '0');
-}
