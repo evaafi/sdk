62686a546249003aca7cf1dd7cc96f694fb33d03
Arsen Andrian <imcrazy.sixtouch@yandex.ru>
2025-10-09 14:35:45 +0800

    rm bigIntToBuffer function

--- src/utils/fivaUtils.ts
+++ src/utils/fivaUtils.ts
@@ -1,4 +1,5 @@
 import { Address } from "@ton/core";
+import { bigIntToBuffer } from "./sha256BigInt";
 
 export function calculateCloseAddress(address: Address, closeToAddress: Address): Address {
   const curHashpart = BigInt('0x' + address.hash.toString('hex'));
@@ -9,15 +10,10 @@ export function calculateCloseAddress(address: Address, closeToAddress: Address)
   const antimask = invertBigInt(mask, 256);
 
   const newHashpart = (curHashpart & mask) | (desiredHashpart & antimask);
-  return new Address(0, bigIntToBuffer(newHashpart, 256));
+  return new Address(0, bigIntToBuffer(newHashpart));
 }
 
 function invertBigInt(value: bigint, bits: number): bigint {
   const maxValue = (1n << BigInt(bits)) - 1n;
   return value ^ maxValue;
-}
-
-function bigIntToBuffer(value: bigint, bits: number): Buffer {
-  const hex = value.toString(16).padStart(bits / 4, '0');
-  return Buffer.from(hex, 'hex');
 }
